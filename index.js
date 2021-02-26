46044c2a4bf984596f3992af8c943d55f1e176f7
Adi Levy <levyadi93@gmail.com>
2021-02-26 11:41:01 +0200

    Adding dynamic creation of elements

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-// Dynamic 2D Matrix
+//! Query Selectors
+const landPage = document.querySelector('#land-page');
+const world = document.querySelector('#world');
+const startBtn = document.querySelector('.start-btn');
+const resetBtn = document.querySelector(".reset-world");
+const mainGame = document.querySelector('.main');
+
+//! Dynamic 2D Matrix
 const matrix = [];
 function matrixBuilder(rowsArg, colsArg) {
+    mainGame.style.gridTemplateRows = `repeat(${rowsArg},1fr)`;
+    mainGame.style.gridTemplateColumns = `repeat(${colsArg},1fr)`;
   for (let rows = 0; rows < rowsArg; rows++) {
     matrix[rows] = [];
     for (let cols = 0; cols < colsArg; cols++) {
@@ -15,57 +24,43 @@ function matrixBuilder(rowsArg, colsArg) {
 }
 const arrayElement = ["sky", "cloud", "grass", "leaves", "wood", "stone","soil"];
 
-
-// Query Selectors
-const landPage = document.querySelector('#land-page');
-const world = document.querySelector('#world');
-const startBtn = document.querySelector('.start-btn');
-const resetBtn = document.querySelector(".reset-world");
-const mainGame = document.querySelector('.main');
-const cloud = document.createElement('div');
-cloud.classList.add("cloud");
-const grass = document.createElement('div');
-grass.classList.add("grass");
-const soil = document.createElement('div');
-soil.classList.add("soil");
-
-// Buttons
+//! Buttons
 startBtn.addEventListener('click', () => {
-    landPage.style.display = 'none';
-    world.style.display = 'flex';
-    matrixBuilder(20,30)
-    injectClouds();
-    injectGrass(15);
-    injectSoil();
+  landPage.style.display = "none";
+  world.style.display = "flex";
+  matrixBuilder(20, 30);
+  //* inject explaining - row,untilRow,col(default = left screen),untilCol(default = right screen),element
+  inject("grass", 15, 16);
+  inject("soil", 16, 20);
+  inject("wood", 12, 15, 23, 24);
+  inject("leaves", 9, 12, 21, 26);
+  
+  //* Creating side soones with for loop
+  for (let i = 0; i < 4; i++) {
+    inject("stone", 11 + i, 15, 0 + i, 1 + i);
+  }
+  
+  //* Creating clouds (row,untilRow,col,untilCol)
+  createClouds(3, 5, 4, 6);
+  createClouds(2, 4, 17, 19);
 })
+
 resetBtn.addEventListener('click', () => {
-    landPage.style.display = "flex";
-    world.style.display = 'none';
-    mainGame.innerHTML = '';
+  landPage.style.display = "flex";
+  world.style.display = 'none';
+  mainGame.innerHTML = '';
 })
 
-
-//! Create clouds - NOT DONE
-function injectClouds() {
-    for (let i = 2; i < 5; i++) {
-        for (let j = 3; j < 8; j+=2) {
-            matrix[i][j].classList.add('cloud');
-        }        
-    }
-}
-// Create Grass
-function injectGrass(row) {
-  for (let i = row; i < row + 1; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      matrix[i][j].classList.value = grass.classList.value;
+//! Dynamic adding elements
+function inject(element,row,untilRow,col = 0,untilCol = matrix[0].length) {
+  for (let i = row; i < untilRow; i++) {
+    for (let j = col; j < untilCol; j++) {
+      matrix[i][j].classList.value = element;
     }
   }
 }
-// Create Soil
-function injectSoil() {
-    for (let i = 16; i < 20; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            matrix[i][j].classList.value = soil.classList.value;            
-        }    
-    }
+function createClouds (row,untilRow,col,untilCol) {
+  for (let i = 0; i < untilRow - row ; i++) {
+    inject("cloud", row + i, untilRow, col - i, untilCol + i);
+  }
 }
